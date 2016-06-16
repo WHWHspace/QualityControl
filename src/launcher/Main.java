19f0ac4d9250c22354d792f27105c02d2e88da52
yuan binhua <yuan binhua>
2016-06-16 13:46:29 +0800

    添加启动类和主窗口

---
+++ src/launcher/Main.java
@@ -0,0 +1,7 @@
+package launcher;
+
+/**
+ * Created by 31344 on 2016/6/16.
+ */
+public class Main {
+}
---
+++ src/ui/MainWindow.java
@@ -0,0 +1,7 @@
+package ui;
+
+/**
+ * Created by 31344 on 2016/6/16.
+ */
+public class MainWindow {
+}
