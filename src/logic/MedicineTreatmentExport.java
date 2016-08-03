334d2efa2742efba080683c05d7cf481b81ccb4d
yuan binhua <yuan binhua>
2016-08-03 16:09:10 +0800

    各个表的导出类

---
+++ src/logic/MedicineTreatmentExport.java
@@ -0,0 +1,43 @@
+package logic;
+
+import model.mysql.Mac_setup;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * Created by 31344 on 2016/8/2.
+ */
+public class MedicineTreatmentExport {
+
+    /**
+     * 导出数据
+     * @param from
+     * @param to
+     * @return
+     */
+    public boolean export(Date from, Date to){
+        return true;
+    }
+
+    /**
+     * 保存数据
+     * @param data
+     * @return
+     */
+    private boolean saveData(List<Mac_setup> data) {
+
+        return true;
+    }
+
+    /**
+     * 读取数据
+     * @param from
+     * @param to
+     * @return
+     */
+    private List<Mac_setup> readData(Date from, Date to){
+        return null;
+    }
+}
